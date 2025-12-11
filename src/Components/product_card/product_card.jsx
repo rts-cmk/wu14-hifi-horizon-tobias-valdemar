7cf84332786e0fcc7eb6848b300b068d0d4877a2
Valdemar Larsen <shido813@gmail.com>
2025-12-11 08:32:20 +0100

    feat: add compareEnabled and stockEnabled props to ProductCard component

--- src/Components/product_card/product_card.jsx
+++ src/Components/product_card/product_card.jsx
@@ -2,13 +2,23 @@ import { GiSettingsKnobs } from "react-icons/gi";
 import { API_BASE_URL } from "../../config/api";
 import "./product_card.scss";
 
-export default function ProductCard({ itemId, name, image, price, stock }) {
+export default function ProductCard({
+  itemId,
+  name,
+  image,
+  price,
+  stock,
+  compareEnabled,
+  stockEnabled,
+}) {
   return (
     <article className="product-card">
-      <div className="product-card__compare">
-        <button className="product-card__compare-button">Compare</button>
-        <GiSettingsKnobs />
-      </div>
+      {compareEnabled && (
+        <div className="product-card__compare">
+          <button className="product-card__compare-button">Compare</button>
+          <GiSettingsKnobs />
+        </div>
+      )}
       <img
         className="product-card__image"
         src={`${API_BASE_URL}/${image}`}
@@ -17,9 +27,11 @@ export default function ProductCard({ itemId, name, image, price, stock }) {
       <h3 className="product-card__name">{name}</h3>
       <p className="product-card__price">{price}</p>
       <button className="product-card__add-to-cart">Add to Cart</button>
-      <p className="product-card__stock">
-        {stock ? `In stock: ${stock}` : "Out of stock"}
-      </p>
+      {stockEnabled && (
+        <p className="product-card__stock">
+          {stock ? `In stock: ${stock}` : "Out of stock"}
+        </p>
+      )}
     </article>
   );
 }
